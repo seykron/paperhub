ef8bcee71dcf10e6229b4097e65995d4efeb318a
Matías MI <matias.mi@riseup.net>
2014-03-30 04:27:15 -0300

    Moved spaguetti code to a proper domain.
    * Removed support for local repositories (it does not scale).
    * Added support for caching (it does scale).
    * Added support to operate directly against Github API.
    * Added support to stateless sessions.

--- paperhub.js
+++ paperhub.js
@@ -1,34 +1,18 @@
 #!/usr/bin/node
 
-var CONVERT_CMD = __dirname + "/compile-document.sh";
-var PAD_URL = process.env.PAD_URL;
-var PAD_API_KEY = process.env.PAD_API_KEY;
+var GithubClient = new require("./lib/GithubClient")
+var GitHubStrategy = require("passport-github").Strategy;
+var Memcached = require('memcached');
 
+var extend = require("extend");
 var express = require('express');
 var engines = require('consolidate');
-var app = express();
-var execFile = require("child_process").execFile;
-var fs = require("fs");
-var git = require("gift");
-var path = require("path");
-var http = require("http");
-var _ = require("underscore");
-var gm = require("gm").subClass({ imageMagick: true });
-var crypto = require("crypto");
 var passport = require("passport");
-var diffReplay = require("diff-replay");
-var jsdiff = require('diff');
-var pad = require('etherpad-lite-client').connect({
-  apikey: PAD_API_KEY,
-  host: process.env.PAD_HOST,
-  port: process.env.PAD_PORT
-});
-var GitHubStrategy = require("passport-github").Strategy;
-var GitHubApi = require("github");
-var github = new GitHubApi({
-  version: "3.0.0",
-  protocol: "https"
-});
+var async = require("async");
+var _ = require("underscore");
+
+var app = express();
+var memcached = new Memcached(process.env.MEMCACHED_URL);
 
 app.use(express.bodyParser({
   keepExtensions: true,
@@ -40,348 +24,170 @@ app.engine('html', engines.handlebars);
 
 // Setup for passport.
 app.use(express.cookieParser());
-app.use(express.session({ secret: 'keyboard cat' }));
+app.use(express.cookieSession({
+  key: "ph.sid",
+  secret: "key cat"
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
-var buildFilesTree = function (root, callback) {
-  var files = [];
-  var pathItem = [];
-
-  var nextNode = function (children, node, fn) {
-    if (children.length === 0) {
-      pathItem.pop();
-      fn(null, files);
-      return;
-    }
-    if (node.contents) {
-      node.contents(function (err, nodes) {
-        if (err) {
-          fn(err, null);
-          return;
-        }
-        pathItem.push(node.name);
-        nextNode(nodes, nodes.shift(),
-          nextNode.bind(null, children, children.shift(), fn));
-      });
-    } else {
-      files.push({
-        fullPath: path.join(pathItem.join("/"), node.name),
-        node: node
-      });
-      nextNode(children, children.shift(), fn);
-    }
-  };
-
-  root.contents(function (err, children) {
-    if (children) {
-      nextNode(children, children.shift(), callback);
-    } else {
-      callback(null, []);
-    }
-  });
-};
-
-var getDocumentId = function (req) {
+var createContext = function (req, callback) {
   var repoName = req.param("repo") || "default";
-  var file = req.param("file") || "untitled-document.md";
-  var revision = req.param("revision");
-  var hash = crypto.createHash("sha1");
-  var documentId;
+  var fileId = req.param("file");
+  var revisionId = req.param("revision");
+  var client = new GithubClient(repoName, req.user);
 
-  if (repoName.substr(-1, 1) !== "/") {
-    repoName += "/";
-  }
-
-  documentId = (repoName + "_" + file).replace(/[\/\\:\.]/ig, "_");
-
-  if (revision) {
-    documentId += "_" + revision;
-  }
-
-  // Hashes the document name, if possible.
-  hash.update(documentId);
-
-  return hash.digest("hex");
-};
-
-var resolveFile = function (req, localRepo) {
-  var file = req.param("file");
-  var fullPath = null;
-
-  if (!file) {
-    fullPath = path.join(localRepo, getDocumentId(req));
-  } else if (file) {
-    fullPath = path.join(localRepo, file.replace(/(\/)*\.\.(\/)*|^\//ig, ''));
-  }
-
-  // Normalizes path, just to avoid scriptkiddies.
-  return fullPath;
-};
-
-var findRevisions = function (repo, file, callback) {
-  var revisions = [];
-  var fullPath = path.join(repo.path, file);
-  var fileExists = function (files) {
-    return files.some(function (item) {
-      return item.fullPath === file;
-    });
-  };
-  var findNext = function (commit, commits) {
-    if (!commit) {
-      callback(null, revisions);
-      return;
-    }
-    buildFilesTree(commit.tree(), function (err, files) {
-      if (err) {
-        callback(err);
-        return;
-      }
-      if (fileExists(files)) {
-        revisions.push({
-          files: files,
-          commit: commit
+  async.waterfall([
+    function initRepos(fn) {
+      client.listRepositories(function (err, repos) {
+        fn(null, {
+          repos: repos
         });
-      }
-      findNext(commits.shift(), commits);
-    });
-  };
-  if (!file) {
-    callback();
-    return;
-  }
-  repo.commits(function (err, commits) {
-    if (err) {
-      callback(err);
-    } else {
-      findNext(commits.shift(), commits);
-    }
-  });
-};
-
-var openRepo = function (req, callback) {
-  var remoteRepo = req.param("repo");
-  var repoName = remoteRepo || "default";
-  var localRepo = path.join(__dirname, "workspace", repoName
-    .replace(/[\/\\:]/ig, "_"));
-  var requiredFile = req.param("file");
-  var proceed = function (repo) {
-    buildFilesTree(repo.tree(), function (err, files) {
-      if (err) {
-        callback(err);
-        return;
-      }
-      findRevisions(repo, req.param("file"), function (err, revisions) {
-        var filesMap = files.map(function (file) {
-          return {
-            repo: remoteRepo,
-            name: file.fullPath
-          };
-        });
-        callback(err, repo, filesMap, resolveFile(req, localRepo), revisions);
       });
-    });
-  };
+    },
+    function initCommonConfig(context, fn) {
+      fn(null, extend(context, {
+        PAD_URL: process.env.PAD_URL,
+        user: req.user
+      }));
+    },
+    function initRepo(context, fn) {
+      client.getContextRepository(function (err, repo) {
+        fn(err, extend(context, {
+          repo: repo
+        }));
+      });
+    },
+    function initFiles(context, fn) {
+      var repo = context.repo;
 
-  if (fs.existsSync(localRepo)) {
-    proceed(git(localRepo));
-  } else {
-    fs.mkdirSync(localRepo);
+      repo.getFiles(repo.default_branch, true, function (err, files) {
+        var currentFile;
 
-    if (remoteRepo) {
-      git.clone(remoteRepo, localRepo, function (err, repo) {
         if (err) {
-          callback(err);
-        } else {
-          proceed(repo);
+          return fn(err);
         }
-      });
-    } else {
-      // Default local repository.
-      git.init(localRepo, function (err, repo) {
-        if (err) {
-          callback(err);
-        } else {
-          proceed(repo);
-        }
-      });
-    }
-  }
-};
-
-var createPadIfNotExists = function (documentId, text, callback) {
-  pad.getLastEdited({
-    padID: documentId
-  }, function (err, data) {
-    if (err) {
-      // Pad does not exist.
-      pad.createPad({
-        padID: documentId
-      }, function (err, data) {
-        if (err) {
-          callback(err);
-          return;
-        }
-        pad.setText({
-          padID: documentId,
-          text: text
-        }, function (err, data) {
-          callback(err);
-        });
-      });
-    } else {
-      callback(null);
-    }
-  });
-};
-var readRevision = function (commit, file, callback) {
-  var revisionFile = _.find(commit.files, function (commitFile) {
-    return file.lastIndexOf(commitFile.fullPath) ===
-      file.length - commitFile.fullPath.length;
-  });
-  var dataStream;
-  var data = "";
-
-  if (!revisionFile) {
-    callback(new Error("File " + file + " not found in revision " + commit.id));
-    return;
-  } else {
-    dataStream = revisionFile.node.dataStream().shift();
-    dataStream.on("data", function (chunk) {
-      data += chunk.toString();
-    });
-    dataStream.on("end", function () {
-      callback(null, data);
-    })
-  }
-};
-var readFile = function (repo, revisions, file, revision, callback) {
-  var commitInfo = _.find(revisions, function (revisionItem) {
-    return revisionItem.commit.id === revision;
-  });
-  if (!commitInfo) {
-    fs.readFile(file, callback);
-    return;
-  }
-  repo.current_commit(function (err, currentCommit) {
-    if (err) {
-      callback(err);
-      return;
-    }
-    readRevision(commitInfo, file, function (err, document) {
-      var relativePath = file.substr(repo.path.length + 1);
-      if (err) {
-        callback(err);
-        return;
-      }
-      repo.diff(currentCommit, commitInfo.commit, [file], function (err, diffs) {
-        var effectiveDocument;
-        var diff;
-
-        if (diffs.length) {
-          diff = diffs.shift().diff; //.replace(/a\//ig, "").replace(/b\//ig, "");
-          console.log(diff);
-          //effectiveDocument = jsdiff.applyPatch(document, diff);
-          effectiveDocument = diffReplay.reverseDiff(document, diff);
-          console.log(effectiveDocument);
-          callback(err, effectiveDocument);
-        } else {
-          callback(err, document);
-        }
-      });
-    });
-  });
-};
-
-var sendResponse = function (req, res, error, files, revisions) {
-  github.repos.getFromUser({
-    user: req.user && req.user.username
-  }, function (err, repos) {
-    res.render("index.html", {
-      error: error,
-      repo: req.param("repo"),
-      gitFiles: files,
-      file: req.param("file"),
-      user: req.user,
-      repos: repos,
-      documentId: getDocumentId(req),
-      PAD_URL: PAD_URL,
-      revisions: revisions
-    });
-  });
-};
-
-app.get('/', function (req, res) {
-  openRepo(req, function (err, repo, files, currentFile, revisions) {
-    var preview = req.param("preview");
-    var page = req.param("page") || 0;
-    var previewFile = currentFile + "." + page + ".png";
-    var outputFile = currentFile + ".pdf";
-    var documentId = getDocumentId(req);
-    var revision = req.param("revision");
-
-    if (fs.existsSync(currentFile)) {
-      if (preview) {
-        gm(outputFile + "[" + page + "]").write(previewFile, function (err) {
-          if (err) {
-            sendResponse(req, res, "Preview does not exist, did you invoked " +
-              "convert first? :)", files, revisions);
-          } else {
-            res.sendfile(previewFile);
-          }
-        });
-      } else {
-        readFile(repo, revisions, currentFile, revision, function (err, document) {
-          if (err) {
-            sendResponse(req, res, err, files, revisions);
+        if (fileId) {
+          currentFile = _.find(files, function (file) {
+            return file.sha === fileId;
+          });
+          if (!currentFile) {
+            fn(new Error("File not found: " + fileId));
             return;
           }
-          createPadIfNotExists(documentId, document, function (err) {
-            sendResponse(req, res, err, files, revisions);
-          });
-        });
-      }
-    } else {
-      sendResponse(req, res, err, files, revisions);
-    }
-  });
-});
-
-app.post("/", function (req, res) {
-  openRepo(req, function (err, repo, files, currentFile, revisions) {
-    var outputFile = currentFile + ".pdf";
-    var params = [currentFile, "-o " + outputFile];
-
-    pad.getText({
-      padID: getDocumentId(req)
-    }, function (err, document) {
-      if (err) {
-        sendResponse(req, res, err, files, revisions);
+        }
+        fn(null, extend(context, {
+          files: files,
+          currentFile: currentFile
+        }));
+      });
+    },
+    function initRevisions(context, fn) {
+      if (!context.currentFile) {
+        fn(null, context);
         return;
       }
+      context.currentFile.getRevisions(function (err, revisions) {
+        var currentRevision;
 
-      fs.writeFileSync(currentFile, document.text);
-
-      execFile(CONVERT_CMD, params, function (error, stdout, stderr) {
-        if (error) {
-          sendResponse(req, res, stderr.toString(), files, revisions);
-        } else {
-          res.download(outputFile, "document.pdf", function (err) {
-            if (err) {
-              sendResponse(req, res, err, files, revisions);
-            }
-          });
+        if (err) {
+          fn(err);
+          return;
         }
+        if (revisionId) {
+          currentRevision = _.find(revisions, function (revision) {
+            return revision.sha === revisionId;
+          });
+          if (!currentRevision) {
+            fn(new Error("Revision not found: " + revisionId));
+            return;
+          }
+        }
+        context.currentFile.initialize(currentRevision && currentRevision.sha,
+          function (err) {
+            fn(err, extend(context, {
+              revisions: revisions,
+              currentRevision: currentRevision
+            }));
+          });
       });
+    }
+  ], function (err, context) {
+    callback(err, context);
+  });
+};
+
+app.get('/preview', function (req, res) {
+  createContext(req, function (err, context) {
+    var page = req.param("page") || 0;
+    var customErr = err;
+
+    if (!context.currentFile) {
+      customErr = new Error("File not specified.");
+    }
+    if (customErr) {
+      res.render("index.html", extend(context, {
+        error: customErr
+      }));
+      return;
+    }
+    context.currentFile.preview(page, function (err, previewFile) {
+      if (err) {
+        res.render("index.html", extend(context, {
+          error: customErr
+        }));
+      } else {
+        res.sendfile(previewFile);
+      }
     });
   });
 });
-passport.serializeUser(function(user, done) {
-  done(null, user);
+
+app.get('/convert', function (req, res) {
+  createContext(req, function (err, context) {
+    var customErr = err;
+
+    if (!context.currentFile) {
+      customErr = new Error("File not specified.");
+    }
+    if (customErr) {
+      res.render("index.html", extend(context, {
+        error: customErr
+      }));
+      return;
+    }
+    context.currentFile.convert(function (err, outputFile) {
+      if (err) {
+        res.render("index.html", extend(context, {
+          error: customErr
+        }));
+      } else {
+        res.download(outputFile, "document.pdf", function (err) {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    });
+  });
 });
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+
+app.get("/", function (req, res) {
+  createContext(req, function (err, context) {
+    res.render("index.html", extend(context, {
+      error: err
+    }));
+  });
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  memcached.get("/accounts/" + id, function (err, user) {
+    done(err, user);
+  });
 });
 passport.use(new GitHubStrategy({
   clientID: process.env.GH_CLIENT_ID,
@@ -389,11 +195,26 @@ passport.use(new GitHubStrategy({
   callbackURL: "http://localhost:7000/auth/github/callback",
   scope: "user,public_repo"
 }, function(accessToken, refreshToken, profile, done) {
-  github.authenticate({
-    type: "oauth",
-    token: accessToken
+  memcached.get("/accounts/" + profile.id, function (err, user) {
+    if (err) {
+      done(err);
+    } else {
+      profile.accessToken = accessToken;
+
+      if (user) {
+        memcached.replace("/accounts/" + profile.id, profile,
+          7200, function (err) {
+            done(err, profile);
+          });
+        done(null, user);
+      } else {
+        memcached.set("/accounts/" + profile.id, profile,
+          7200, function (err) {
+            done(err, profile);
+          });
+      }
+    }
   });
-  done(null, profile);
 }));
 
 app.get('/auth/github', passport.authenticate('github'));
